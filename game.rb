9052ded87de2995cbccdbe805adef2cb337269ee
parentda <danielparent21@gmail.com>
2021-08-26 11:08:53 -0400

    Added game_roles method to properly set codebreaker/codemaker to human/computer objects

--- game.rb
+++ game.rb
@@ -1,14 +1,39 @@
 # frozen_string_literal: true
+require_relative 'displayable'
+require_relative 'board'
+require_relative 'human'
+require_relative 'computer'
 
 class Game
+  include Display
   attr_reader :turn
 
   def initialize
     @turn_number = 1
     @max_turns = 12
+    @game_mode_options = %w[1 2]
   end
 
-  def play_game; end
+  def play_game(game_mode)
+    @game_mode = game_mode
+    setup
+  end
 
-  def game_setup; end
+  def setup
+    @board = Board.new
+    game_roles
+  end
+
+  def game_roles
+    case @game_mode
+    when @game_mode_options[0]
+      @codebreaker = Human.new
+      @codemaker = Computer.new
+    when @game_mode_options[1]
+      @codebreaker = Computer.new
+      @codemaker = Human.new
+    else
+      puts 'Error, invalid selection'
+    end
+  end
 end
